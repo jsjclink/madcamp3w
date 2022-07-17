477a5a91a0cb741238239246598c3d65cb10909a
suhyeonPark <0111suhyeon@kaist.ac.kr>
2022-07-18 01:15:31 +0900

    랜덤으로 줌아웃+model 랜덤모션 넣음

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -6,15 +6,49 @@ public class FollowPlayer : MonoBehaviour
 {
     [SerializeField]
     public GameObject player;
+    [SerializeField]
+    private GameObject model;
+
+    private GameObject clone;
+    private int term = 3;
+    private float time = 0.0f;
+    private bool isappeared = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        term = Random.Range(3,5);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        time += Time.deltaTime;
+        
+        if (time > term){
+            if (transform.GetComponent<Camera>().orthographicSize <20){
+                transform.GetComponent<Camera>().orthographicSize += 0.05f;
+            }
+            if (time > 5 && !isappeared){
+                isappeared = true;
+                Debug.Log("appeared");
+                clone = Instantiate(model, new Vector3(3,3,-1), Quaternion.identity);
+                Destroy(clone, 3.0f);
+            }
+            else if (time > 10){
+                isappeared = false;
+                transform.GetComponent<Camera>().orthographicSize = 5;
+                term = Random.Range(3,5);
+                time = 0.0f;
+            }
+            
+        }
         transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -100);
+        
+
+        
+
     }
+
 }
+
